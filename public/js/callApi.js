6d2b4cb82228c5415ba31cd7713868ec14599569
daingo <daingo7200@gmail.com>
2021-11-10 09:27:03 +0700

    save 6

--- public/js/callApi.js
+++ public/js/callApi.js
@@ -1,8 +1,7 @@
+// Test lấy data
 document.getElementById('getData').onclick = async () => {
-	const a = Date.now();
-	const data = await axios.get('http://localhost:3000/api/time');
-	console.log(a);
-	[...data.data].forEach((element) => {
+	const arr = await axios.get('http://localhost:3000/api/time');
+	arr.data.forEach((element) => {
 		if (
 			Date.parse(element.createdAt) > Date.parse('2021-11-07 17:') &&
 			Date.parse(element.createdAt) < Date.parse('2021-11-07 18:')
@@ -32,30 +31,6 @@ document.getElementById('getData').onclick = async () => {
 	// 			);
 	// 		})
 	// 	);
-	// let u = 0;
-	// let i = 0;
-	// data.forEach((item) => {
-	// 	if (item.volt && item.amp) {
-	// 		u += item.volt;
-	// 		i += item.amp;
-	// 	} else {
-	// 		fetch('http://localhost:3000/api/delete/date', {
-	// 			method: 'DELETE',
-	// 			mode: 'cors',
-	// 			cache: 'no-cache',
-	// 			credentials: 'same-origin',
-	// 			headers: {
-	// 				'Content-Type': 'application/json',
-	// 			},
-	// 			referrerPolicy: 'no-referrer',
-	// 			body: JSON.stringify({
-	// 				date: item.createdAt,
-	// 			}),
-	// 		});
-	// 	}
-	// });
-	// console.log(u);
-	// console.log(i);
 };
 
 document.getElementById('date').onblur = async (e) => {
@@ -66,53 +41,13 @@ document.getElementById('date').onblur = async (e) => {
 			date: e.target.value,
 		},
 	});
+
+	// Tách giờ trong ngày
 	const arrDate = [...value.data];
-	let isCheckLess = false;
-	let isCheckGreater = false;
-	// const a = value.data[890].createdAt;
-	// const b = new Date(a).getTime();
-	// const c = new Date(a).setHours(20);
-	// console.log(c);
-	// console.log(b);
-	// console.log(b > c);
-	// console.log(new Date(c));
-	// console.log(new Date(b));
-	const arrHour = [];
-	const length = value.data.length;
-	const start = new Date(value.data[0].createdAt);
-	const end = new Date(value.data[length - 1].createdAt);
-	const TOTAL_MINUTE = 59;
-	const startHour = start.getHours();
-	const startMinute = start.getMinutes();
-	const endHour = end.getHours();
-	const endMinute = end.getMinutes();
-	const preIndex = 0;
-	for (let i = TOTAL_MINUTE - startMinute; i < length; i += 59) {
-		do {
-			if (
-				new Date(arrDate[i].createdAt).getTime() <
-				new Date(arrDate[i].createdAt).setHours(
-					new Date(arrDate[i].createdAt).getHours() + 1
-				)
-			) {
-				isCheckLess = true;
-				if (isCheckGreater) {
-					return i;
-				}
-				i++;
-			} else {
-				isCheckGreater = true;
-				i--;
-				if (isCheckLess) {
-					return i;
-				}
-			}
-		} while (!(isCheckLess && isCheckGreater));
-		arrHour = arrHour.push(value.data.splice(0, i - preIndex + 1));
-		preIndex = i;
-	}
+	const arrHour = sliceHour(arrDate, value.data);
 	console.log(arrHour);
 };
+
 document.getElementById('hour').onblur = async (e) => {
 	const data = await axios({
 		method: 'post',
@@ -123,3 +58,47 @@ document.getElementById('hour').onblur = async (e) => {
 	});
 	console.log(data.data);
 };
+
+function sliceHour(arrDate, pureArr) {
+	const arrHour = [];
+	const length = arrDate.length;
+	const start = new Date(arrDate[0].createdAt);
+	const TOTAL_MINUTE = 59;
+	let startHour = start.getHours();
+	let startMinute = start.getMinutes();
+	let preIndex = 0;
+
+	for (
+		let i = TOTAL_MINUTE - startMinute;
+		i < length || pureArr.length !== 0;
+		i += 59
+	) {
+		let isCheckLess = false;
+		let isCheckGreater = false;
+
+		if (i > length) {
+			i = length - 1;
+			isCheckLess = true;
+			isCheckGreater = true;
+		}
+		do {
+			if (
+				new Date(arrDate[i].createdAt).getTime() <
+				new Date(arrDate[i].createdAt).setHours(startHour + 1, 0, 0, 0)
+			) {
+				isCheckLess = true;
+				if (!isCheckGreater) {
+					i++;
+				}
+			} else {
+				isCheckGreater = true;
+				i--;
+			}
+		} while (!(isCheckLess && isCheckGreater));
+		arrHour.push(pureArr.splice(0, i - preIndex + 1));
+		preIndex = i + 1;
+		if (pureArr.length !== 0)
+			startHour = new Date(pureArr[0].createdAt).getHours();
+	}
+	return arrHour;
+}
